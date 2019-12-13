bf4ea79db0e1fac54e75600016964d588f111acc
Abe M <kinofevil@gmail.com>
2019-12-13 02:21:45 +0000

    more pretty

--- start2.py
+++ start2.py
@@ -12,37 +12,25 @@ dict_of_combos = {state: [] for state in list_of_states}
 
 
 with open('stid.csv', 'r') as csv_file:
-    data_csv = list(csv.DictReader(csv_file))
-    states_ui_sep_ui = [row ['State'] for row in data_csv if row['Unemployment'] == 'Seperate Unemployment Registration']
-    states_ui_same_wh = [row ['State'] for row in data_csv if row['Unemployment'] == 'On Withholding Registration']
-    states_ui_same_str = [row ['State'] for row in data_csv if row['Unemployment'] == 'On Sales Tax Registration']
-    states_wh_same_str = [row ['State'] for row in data_csv if row['Withholding'] == 'On Sales Tax Registration']
-    states_wh_sep_wh = [row ['State'] for row in data_csv if row['Withholding'] == 'Seperate Withholding Registration']
-    states_immediate_ui = [row ['State'] for row in data_csv if row['Do we currently get the UIID at the time of registration?'] == 'Yes']
-    states_immediate_wh = [row ['State'] for row in data_csv if row['Do we currently get the WHID at the time of registration?'] == 'Yes']
+    data_csv = list(csv.DictReader(csv_file)) 
+    
     for state in list_of_states:
-        if state in states_ui_sep_ui:
-            dict_of_combos[state].append(ui_status='sep_ui')
-        elif state in states_ui_same_wh:
-            dict_of_combos[state].append(ui_status='ui_with_wh')
-        elif state in states_ui_same_str:
-            dict_of_combos[state].append(ui_status='ui_with_str')
+        if [row ['State'] for row in data_csv if row['Unemployment'] == 'Seperate Unemployment Registration']:
+            dict_of_combos[state].append('sep_ui')
+        elif [row ['State'] for row in data_csv if row['Unemployment'] == 'On Withholding Registration']:
+            dict_of_combos[state].append('ui_with_wh')
+        elif [row ['State'] for row in data_csv if row['Unemployment'] == 'On Sales Tax Registration']:
+            dict_of_combos[state].append('ui_with_str')
+        elif [row ['State'] for row in data_csv if row['Withholding'] == 'On Sales Tax Registration']:
+            dict_of_combos[state].append('wh_with_str')
+        elif [row ['State'] for row in data_csv if row['Withholding'] == 'Seperate Withholding Registration']:
+            dict_of_combos[state].append('sep_wh')
+        elif [row ['State'] for row in data_csv if row['Do we currently get the UIID at the time of registration?'] == 'Yes']:
+            dict_of_combos[state].append('immediate_ui')
+        elif [row ['State'] for row in data_csv if row['Do we currently get the WHID at the time of registration?'] == 'Yes']:
+            dict_of_combos[state].append('immediate_wh')
         else:
-            dict_of_combos[state].append(ui_status='none')
-
-        if state in states_wh_same_str:
-            dict_of_combos[state].append(wh_status='wh_with_str')
-        elif state in states_wh_sep_wh:
-            dict_of_combos[state].append(wh_status='sep_wh')
-        else:
-            dict_of_combos[state].append(wh_status='none') 
-        
-        if state in states_immediate_ui:
-            dict_of_combos[state].append(immediacy_status='immediate_ui')
-        elif state in states_immediate_wh:
-            dict_of_combos[state].append(immediacy_status='immediate_wh')
-        else:
-            dict_of_combos[state].append(immediacy_status='none')
+            dict_of_combos[state].append('none')
 
 for state in list_of_states:
     user_input = input(f'Do you want to skip this state? {state}  |   y/n:')
@@ -84,7 +72,7 @@ for state in list_of_states:
                 'next_steps_ui' : input(f'UI: Next Steps for {state}:')
                 }
     
-        dict_of_combos[state].append(context_full={**context_ui, **context_wh, **context_str})
+        dict_of_combos[state].append({**context_ui, **context_wh, **context_str})
     
     elif user_input == 'y':
         print(f'{state} not templated')
@@ -93,107 +81,63 @@ for state in list_of_states:
         print('please enter y or n')
 
 
+
+def templatee(state, inputfile, outputfile):
+    fulldoc = DocxTemplate(f'{inputfile}_template.docx')
+    fulldoc.render(context)
+    fulldoc.save(f'{outputfile}_{state}.docx')
+
+
+
 for state in list_of_states:
-    context = 3
+    context = dict_of_combos[state][3] # 3 is index 3 of the context list we created up above
     if 'skip' in dict_of_combos[state]:
         continue
     elif 'ui_with_str' in dict_of_combos[state] and 'wh_with_str' in dict_of_combos[state]:
-        # immediate_ui = get immediate ui immediate_wh = get immediate wh
         if 'immediate_ui' in dict_of_combos[state] or 'immediate_wh' in dict_of_combos[state]:
-            fulldoc = DocxTemplate('FULL_imme_template.docx')
-            fulldoc.render(dict_of_combos[state][context])
-            fulldoc.save(f'FULL_{state}.docx')
-            fulldoc = DocxTemplate('ADP_template.docx')
-            fulldoc.render(dict_of_combos[state][context])
-            fulldoc.save(f'ADP_{state}.docx')
-            fulldoc = DocxTemplate('WHUI_imme_template.docx')
-            fulldoc.render(dict_of_combos[state][context])
-            fulldoc.save(f'WHUI_{state}.docx')
-
+            self.templatee(state, 'FULL_imme', 'FULL')
+            self.templatee(state, 'ADP', 'ADP')
+            self.templatee(state, 'WHUI_imme', 'WHUI')
 
         else:
-            fulldoc = DocxTemplate('FULL_nonim_template.docx')
-            fulldoc.render(dict_of_combos[state][context])
-            fulldoc.save(f'FULL_{state}.docx')
-            fulldoc = DocxTemplate('ADP_template.docx')
-            fulldoc.render(dict_of_combos[state][context])
-            fulldoc.save(f'ADP_{state}.docx')
-            fulldoc = DocxTemplate('WHUI_nonim_template.docx')
-            fulldoc.render(dict_of_combos[state][context])
-            fulldoc.save(f'WHUI_{state}.docx')
+            self.templatee(state, 'FULL_nonim', 'FULL')
+            self.templatee(state, 'ADP', 'ADP')
+            self.templatee(state, 'WHUI_nonim', 'WHUI')
 
     elif ('sep_ui' in dict_of_combos[state] and 'wh_with_str' in dict_of_combos[state]) or ('sep_ui' in dict_of_combos[state] and 'sep_wh' in dict_of_combos[state]):
         if 'immediate_ui' in dict_of_combos[state] and 'immediate_wh' in dict_of_combos[state]:
-            fulldoc = DocxTemplate('UI_imme_template.docx')
-            fulldoc.render(dict_of_combos[state][context])
-            fulldoc.save(f'UI_{state}.docx')
-            fulldoc = DocxTemplate('ADP_template.docx')
-            fulldoc.render(dict_of_combos[state][context])
-            fulldoc.save(f'ADP_{state}.docx')
-            fulldoc = DocxTemplate('WH_imme_template.docx')
-            fulldoc.render(dict_of_combos[state][context])
-            fulldoc.save(f'WH_{state}.docx')
-            fulldoc = DocxTemplate('WHUI_imme_template.docx')
-            fulldoc.render(dict_of_combos[state][context])
-            fulldoc.save(f'WHUI_{state}.docx')
+            self.templatee(state, 'UI_imme', 'UI')
+            self.templatee(state, 'ADP', 'ADP')
+            self.templatee(state, 'WH_imme', 'WH')
+            self.templatee(state, 'WHUI_imme', 'WHUI')
+
         
         elif 'immediate_ui' in dict_of_combos[state] and not 'immediate_wh' in dict_of_combos[state]:
-            fulldoc = DocxTemplate('UI_imme_template.docx')
-            fulldoc.render(dict_of_combos[state][context])
-            fulldoc.save(f'UI_{state}.docx')
-            fulldoc = DocxTemplate('ADP_template.docx')
-            fulldoc.render(dict_of_combos[state][context])
-            fulldoc.save(f'ADP_{state}.docx')
-            fulldoc = DocxTemplate('WH_nonim_template.docx')
-            fulldoc.render(dict_of_combos[state][context])
-            fulldoc.save(f'WH_{state}.docx')
-            fulldoc = DocxTemplate('WHUI_nonim_template.docx')
-            fulldoc.render(dict_of_combos[state][context])
-            fulldoc.save(f'WHUI_{state}.docx')
+            self.templatee(state, 'UI_imme', 'UI')
+            self.templatee(state, 'ADP', 'ADP')
+            self.templatee(state, 'WH_nonim', 'WH')
+            self.templatee(state, 'WHUI_nonim', 'WHUI')
+            
         
         elif 'immediate_wh' in dict_of_combos[state] and not 'immediate_ui' in dict_of_combos[state]:
-            fulldoc = DocxTemplate('UI_nonim_template.docx')
-            fulldoc.render(dict_of_combos[state][context])
-            fulldoc.save(f'UI_{state}.docx')
-            fulldoc = DocxTemplate('ADP_template.docx')
-            fulldoc.render(dict_of_combos[state][context])
-            fulldoc.save(f'ADP_{state}.docx')
-            fulldoc = DocxTemplate('WH_imme_template.docx')
-            fulldoc.render(dict_of_combos[state][context])
-            fulldoc.save(f'WH_{state}.docx')
-            fulldoc = DocxTemplate('WHUI_nonim_template.docx')
-            fulldoc.render(dict_of_combos[state][context])
-            fulldoc.save(f'WHUI_{state}.docx')
+            self.templatee(state, 'UI_nonim', 'UI')
+            self.templatee(state, 'ADP', 'ADP')
+            self.templatee(state, 'WH_imme', 'WH')
+            self.templatee(state, 'WHUI_nonim', 'WHUI')
         
         else:
-            fulldoc = DocxTemplate('UI_nonim_template.docx')
-            fulldoc.render(dict_of_combos[state][context])
-            fulldoc.save(f'UI_{state}.docx')
-            fulldoc = DocxTemplate('ADP_template.docx')
-            fulldoc.render(dict_of_combos[state][context])
-            fulldoc.save(f'ADP_{state}.docx')
-            fulldoc = DocxTemplate('WH_nonim_template.docx')
-            fulldoc.render(dict_of_combos[state][context])
-            fulldoc.save(f'WH_{state}.docx')
-            wfulldoc =DocxTemplate('WHUI_nonim_template.docx')
-            fulldoc.render(dict_of_combos[state][context])
-            fulldoc.save(f'WHUI_{state}.docx')
+            self.templatee(state, 'UI_nonim', 'UI')
+            self.templatee(state, 'ADP', 'ADP')
+            self.templatee(state, 'WH_nonim', 'WH')
+            self.templatee(state, 'WHUI_nonim', 'WHUI')
 
     elif 'ui_with_wh' in dict_of_combos[state]:
         if 'immediate_ui' in dict_of_combos[state] or 'immediate_wh' in dict_of_combos[state]:
-            fulldoc = DocxTemplate('WHUI_imme_template.docx')
-            fulldoc.render(dict_of_combos[state][context])
-            fulldoc.save(f'WHUI_{state}.docx')
-            fulldoc = DocxTemplate('ADP_template.docx')
-            fulldoc.render(dict_of_combos[state][context])
-            fulldoc.save(f'ADP_{state}.docx')
+            self.templatee(state, 'WHUI_imme', 'WHUI')
+            self.templatee(state, 'ADP', 'ADP')
         else:
-            fulldoc = DocxTemplate('WHUI_nonim_template.docx')
-            fulldoc.render(dict_of_combos[state][context])
-            fulldoc.save(f'WHUI_{state}.docx')
-            fulldoc = DocxTemplate('ADP_template.docx')
-            fulldoc.render(dict_of_combos[state][context])
-            fulldoc.save(f'ADP_{state}.docx')
+            self.templatee(state, 'WHUI_nonim', 'WHUI')
+            self.templatee(state, 'ADP', 'ADP')
 
     else:
         print(f'{state} failed to template.')
